0bb4b9df2a85cd6a0ceded9b25f5ee50669a060d
ahsan28 <ahabib.j@gmail.com>
2022-09-07 16:34:51 +0600

    fix server url h

--- src/services/media.services.js
+++ src/services/media.services.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth.header';
 
-const api = "https://rjrydrug.herokuapp.com/"||'http://localhost:5001/media/'; //'http://localhost:5000/media/'||"https://rjrydrug.herokuapp.com/"
+const api = "https://rjrydrug.herokuapp.com/"||'http://localhost:5000/media/'; //'http://localhost:5000/media/'||"https://rjrydrug.herokuapp.com/"
 
 const read = (id) => {
     console.log('MediaService.js')
